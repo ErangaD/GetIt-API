c659a00ac4a4f83909bbfe36d168829473f8c4d0
erangad <erangadulshan.14@cse.mrt.ac.lk>
2017-05-16 17:09:27 +0530

    make it bettr

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var admin = require('./routes/adminRoutes');
 //set our port to either a predetermined port number if you have set it up, or 3001
 var port = process.env.API_PORT || 3001;
 
-app.use(express.static(path.join(__dirname,'/dist')));
+app.use(express.static(path.join(__dirname,'/build')));
 //db config
 //mongodb://localhost/get_it_application
 mongoose.connect('mongodb://ErangaD:Erangadulshan10@ds143181.mlab.com:43181/getitlk');
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 //To prevent errors from Cross Origin Resource Sharing, we will set
 //our headers to allow CORS with middleware like so:
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
@@ -54,10 +54,11 @@ app.use('/api/user',profile);
 app.use('/api/admin',admin);
 app.use('/api', registration);
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'/dist/index.html'));
+    res.sendFile(path.join(__dirname,'/build/index.html'));
 });
 //starts the server and listens for requests
 http.listen(port, function() {
     console.log(`api running  on port ${port}`);
 });
 module.exports = express;
+//react-scripts start
